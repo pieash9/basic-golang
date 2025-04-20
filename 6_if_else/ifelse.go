6913e05ff8313aac28edcad79537ff0c062302b9
Md. Pieash Ahmed <pieash9@gmail.com>
2025-04-20 12:01:38 +0600

    if else

---
+++ 6_if_else/ifelse.go
@@ -0,0 +1,39 @@
+package main
+
+func main() {
+	// age := 16
+
+	// if age >= 18 {
+	// 	fmt.Println("You can vote")
+	// } else {
+	// 	fmt.Println("You cannot vote")
+	// }
+
+	// age := 11
+
+	// if age >= 18 {
+	// 	println("Person is an adult")
+	// } else if age > 12 {
+	// 	println("Person is a teenage")
+	// } else {
+	// 	println("Person is a kid!")
+	// }
+
+	// var role = "admin"
+	// var hasPermission = false
+
+	// if role == "admin" && hasPermission {
+	// 	println("User is an admin")
+	// } else {
+	// 	println("User is not an admin")
+	// }
+
+	// we can declare a variable inside the if block
+	// if age := 20; age > 18 {
+	// 	println("Person is an adult", age)
+	// } else if age >= 12 {
+	// 	println("Person is a teenage", age)
+	// }
+
+	// go does not have ternary operator , you will have to use normal if else
+}
