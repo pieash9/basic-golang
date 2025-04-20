cabbe4e6ed5ccb22a531fde077b7f8d9fa0cf2e7
Md. Pieash Ahmed <pieash9@gmail.com>
2025-04-20 22:57:11 +0600

    variadic function

--- 13_variadic_function/variadic_function.go
+++ 13_variadic_function/variadic_function.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func sum(nums ...int) int {
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+
+	return total
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	result := sum(nums...)
+	fmt.Println(result)
 }
