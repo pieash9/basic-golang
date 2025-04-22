65a5cbdf1c2f3a231de9b47a70fbfdc91bf728f8
Md. Pieash Ahmed <pieash9@gmail.com>
2025-04-22 22:38:18 +0600

    files

--- 23_files/files.go
+++ 23_files/files.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -40,9 +41,92 @@ func main() {
 	// 	println("data", d, string(buf[i]))
 	// }
 
-	data, err := os.ReadFile("example.txt")
+	// data, err := os.ReadFile("example.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// println(string(data))
+
+	//  read folders
+
+	// dir, err := os.Open("../")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer dir.Close()
+
+	// fileInfo, err := dir.ReadDir(-1)
+
+	// for _, fi := range fileInfo {
+	// 	fmt.Println(fi.Name(), fi.IsDir())
+	// }
+
+	// create a file
+	// f, err := os.Create("example2.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+
+	// f.WriteString("Hi Go!")
+	// f.WriteString("Nice language!")
+
+	// bytes := []byte("Hello go language!")
+	// f.Write(bytes)
+
+	// read and write in another file (streaming fashion)
+
+	// sourceFile, err := os.Open("example.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer sourceFile.Close()
+
+	// destFile, err := os.Create("example2.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer destFile.Close()
+
+	// reader := bufio.NewReader(sourceFile)
+	// writer := bufio.NewWriter(destFile)
+
+	// for {
+	// 	b, err := reader.ReadByte()
+	// 	if err != nil {
+	// 		if err.Error() != "EOF" {
+	// 			panic(err)
+	// 		}
+	// 		break
+	// 	}
+
+	// 	e := writer.WriteByte(b)
+	// 	if e != nil {
+	// 		panic(e)
+	// 	}
+	// }
+
+	// writer.Flush()
+
+	// fmt.Println("Written to new file successfully!")
+
+	// delete a  file
+	// sourceFile, err := os.Open("example2.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer sourceFile.Close()
+
+	// delete file
+	err := os.Remove("example2.txt")
+
 	if err != nil {
 		panic(err)
 	}
-	println(string(data))
+
+	fmt.Println("File deleted!")
 }
